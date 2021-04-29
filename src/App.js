bcfcd8dd6a511c062fb038a4693546d84518793d
realkelvinperez <realkelvinperez@gmail.com>
2021-04-29 00:13:10 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
+import {useEffect, useState} from 'react'
+import { Global, css } from "@emotion/react";
+import VConsole from "vconsole";
+
+import {
+    Box,
+    Button,
+    Container,
+    Flex,
+    Heading
+} from "@chakra-ui/react";
 
 function App() {
+    const [address, setAddress] = useState('')
+    const [accounts, setAccounts] = useState('')
+
+    const handleClick = async () => {
+        if(window?.ethereum){
+            const accounts = await window.ethereum.send('eth_requestAccounts')
+            setAccounts(accounts)
+            setAddress(accounts.result[0])
+        }
+        else alert('No Ethereum Provider')
+    }
+
+    useEffect(() => {
+        if(window) new VConsole();
+    })
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <>
+          <Global
+              styles={
+                  css`
+                     body, #__next, #__next div {
+                       min-height: 100vh;
+                       min-height: -webkit-fill-available;
+                     }
+                `
+              }
+          />
+          <Container as="main">
+              <Flex
+                  direction={'column'}
+                  alignItems={'center'}
+                  justifyContent={'center'}
+              >
+                  <Heading marginY={1}>ViViFi Capital</Heading>
+                  <Heading as="h3" size="sm">Dashboard</Heading>
+
+                  <Button marginY={4} onClick={handleClick}>
+                      {!address ? 'Connect Wallet' : 'Wallet Connected'}
+                  </Button>
+
+                  <Heading as="h3" size="sm">Account Address</Heading>
+                  <Box>{address}</Box>
+              </Flex>
+          </Container>
+      </>
   );
 }
 
