90ad968a1cb631c4e0249c7cbdcf3a2d63521f93
realkelvinperez <realkelvinperez@gmail.com>
2021-05-01 22:04:28 -0400

    preparing for new design

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import {useEffect, useState} from 'react'
 import { Global, css } from "@emotion/react";
 import VConsole from "vconsole";
 import Web3 from "web3";
-// import axios from "axios"
+import axios from "axios"
 
 // 56 - BSC Main Net
 // 97 - BSC Test Net
@@ -17,38 +17,39 @@ import {
 
 
 function App() {
-    const [address, setAddress] = useState(null)
-    const [accounts, setAccounts] = useState(null)
-    const [chainID, setChainID] = useState(null)
+    const [address, setAddress] = useState(undefined)
+    const [isLoading, setIsLoading] = useState(false)
+    const [chainID, setChainID] = useState(undefined)
     const [balance, setBalance] = useState(0)
-    const [web3, setWeb3] = useState(null)
+    const [web3, setWeb3] = useState(undefined)
+    // const [tokenTx, setTokenTx] = useState([])
 
-    // const [isLoading, setLoading] = useState(true)
-    // const BSC_APIKEY = 'C8DVY3VCSKTFFBXTD11K712YP9T3GYT4PX'
-    // const apiUrl = `https://api.bscscan.com/api?module=account&action=balance&address=${address}&tag=latest&apikey=${BSC_APIKEY}`
-    // const testnetApiUrl = `https://api-testnet.bscscan.com/api?module=account&action=balance&address=${address}&tag=latest&apikey=${BSC_APIKEY}`
-    // axios.get(testnetApiUrl).then(async ({ data }) => {
-    //     console.log({data})
-    // })
-    // const userBalance = web3.utils.fromWei(ethBalance)
+    const BSC_APIKEY = 'C8DVY3VCSKTFFBXTD11K712YP9T3GYT4PX'
+    // const apiUrl = `https://api.bscscan.com/api?module=account&action=balance&address=${address}&tag=latest&apikey=${BSC_APIKEY}` // account balance
+    // const testnetApiUrl = `https://api-testnet.bscscan.com/api?module=account&action=balance&address=${address}&tag=latest&apikey=${BSC_APIKEY}` // test net account balance
 
     const handleClick = async () => {
         if(window?.ethereum){
+            setIsLoading(true)
             const { eth } = web3
             const address = await eth.getCoinbase(); // coinbase gets the users address
             setChainID(await eth.getChainId())
-            setAccounts(await eth.getAccounts())
             setAddress(address)
             const ethBalance = await eth.getBalance(address) // get balance gets the users current balance
-            debugger;
             setBalance(parseFloat(web3.utils.fromWei(ethBalance)).toFixed(2))
-            console.log({ethBalance})
+            setIsLoading(false)
+            const testAddresss = '0x72750c3036aaad52fc4c9cf0a4fff36fa11db372'
+            const addressTokens = `https://api.bscscan.com/api?module=account&action=tokentx&address=${testAddresss}&sort=desc&apikey=${BSC_APIKEY}`
+            axios.get(addressTokens).then(async ({ data : { result } }) => {
+                console.log({result})
+                // setTokenTx(result)
+            })
         }
         else alert('No Ethereum Provider')
     }
 
     useEffect(() => {
-        if(typeof window?.ethereum !== "undefined") {
+        if(typeof window?.ethereum !== "undefined" && typeof address === 'undefined') {
             setWeb3(new Web3(window.ethereum))
             console.log('Set web3')
             if(typeof VConsole !== "undefined") new VConsole()
@@ -56,7 +57,8 @@ function App() {
                 console.log('MetaMask is installed!');
             }
         }
-    },[])
+
+    },[address])
 
   return (
       <>
@@ -81,13 +83,11 @@ function App() {
               <Flex
                   direction={'column'}
                   alignItems={'center'}
-                  justifyContent={'center'}
                   css={css`
                     height: 100%;
                   `}
               >
                   <Heading marginY={1}>ViViFi Capital</Heading>
-                  <Heading as="h3" size="sm">Dashboard</Heading>
 
                   <Button marginY={4} onClick={handleClick}>
                       {!address ? 'Connect Wallet' : 'Wallet Connected'}
@@ -99,7 +99,7 @@ function App() {
                   {chainID === 56 && (<Box>Binance Mainnet Chain</Box>)}
                   {chainID === 97 && (<Box>Binance Testnet Chain</Box>)}
                   <Heading as="h3" size="sm">Balance</Heading>
-                  <Box>{balance}</Box>
+                  {!isLoading ? (<Box>{balance} BNB</Box>) : 'Loading...'}
               </Flex>
           </Container>
       </>
